b2d1c2d7c65e59498762c8329412e068fcfb08d9
Adriana Tihan <fvwg34@durham.ac.uk>
2021-01-07 14:03:54 +0200

    Changes to server, moved data to JSON file

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
 const express = require('express');
 const app = express();
-//const fs = require('fs');
+const fs = require('fs');
 app.use(express.static('client'));
 app.use(express.json()); //Parse JSON-encoded bodies
 
-let books = ['The Great Gatsby',
-'Harry Potter'];
+//let list = ['The Great Gatsby','Harry Potter'];
 
-//let rawdata = fs.readFileSync('books.json');
-//let library = JSON.parse(rawdata);
-let library={
-    "books":[
-        {
-            "title": "The Great Gatsby",
-            "author": "F. Fitzgerald",
-            "review": "Nice"
-        },
-        {
-            "title": "Harry Potter",
-            "author": "J.K. Rowling",
-            "review": "Good"
-        }
-    ]
-}
+
+let rawdata = fs.readFileSync('./books.json');
+let library = JSON.parse(rawdata);
+
+/*let list=[];
+for(let book of library.books){
+    list.push(book);
+}*/
+
 
 app.get('/books/list', function(req, resp){
   resp.json(library);
@@ -32,10 +24,16 @@ app.post('/books/add', function(req, resp){
   const newbook = req.body.newbook;
   const newauthor = req.body.newauthor;
   const newreview = req.body.newreview;
-  let obj= { "title": newbook,
-        "author": newauthor,
-        "review": newreview};
+  let obj= { title: newbook,
+        author: newauthor,
+        review: newreview};
   library.books.push(obj);
+  fs.writeFile('./books.json',JSON.stringify(library, null, 2), err =>{
+      if(err){
+          console.log(err);
+      }
+  });
+  //list.push(newbook);
   resp.json(library);
 });
 
