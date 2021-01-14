f7024e6cebd469593d9ef33daf2a3776bbd5e8b3
Adriana Tihan <fvwg34@durham.ac.uk>
2021-01-14 15:25:49 +0200

    Added list of contributors, created JSOn file of users

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ const fs = require('fs');
 app.use(express.static('client'));
 app.use(express.json()); //Parse JSON-encoded bodies
 
-//let list = ['The Great Gatsby','Harry Potter'];
 
 
-let rawdata = fs.readFileSync('./books.json');
-let library = JSON.parse(rawdata);
 
-/*let list=[];
-for(let book of library.books){
-    list.push(book);
-}*/
+let rawdata1 = fs.readFileSync('./books.json');
+let library = JSON.parse(rawdata1);
 
+let rawdata2 = fs.readFileSync('./people.json');
+let people = JSON.parse(rawdata2);
 
 app.get('/books/list', function(req, resp){
   resp.json(library);
+});
+
+app.get('/people/list', function(req, resp){
+    resp.json(people);
 });
 
 app.post('/books/add', function(req, resp){
--- client/index.js
+++ client/index.js
@@ -1,29 +1,71 @@
+
 window.addEventListener('load', async function(event){
-  let response = await fetch('http://127.0.0.1:8090/books/list');
+  let response = await fetch('http://127.0.0.1:8090/people/list');
   let body = await response.json();
-  renderBooks(body);  
+  renderPeople(body);
 });
 
+function renderPeople(people){
+  let container = document.getElementById('people');
+    container.innerHTML = "<h4>Our contributors are:</h4>";
+    for(let person of people.people){
+      let item = document.createElement('div');
+      item.innerHTML= person.lname + ',' + person.fname+ '<br>';
+    container.appendChild(item);
+    }
+}
+
+let login = document.getElementById('LogInSubmit');
+login.addEventListener('click', async function(event){
+
+  event.preventDefault();
+
+  let response = await fetch('http://127.0.0.1:8090/books/list');
+  let body = await response.json();
+  renderBooks(body);
+
+});
+
+function addForm(){
+
+  bookform=
+  '<h4>Add books:</h4>'+
+  "<form action='http://127.0.0.1:8090/books/add' method='post'>"+
+  '<label for="newbook">Book title:</label>'+
+  "<input id ='newbook' type='text'><br>"+
+  '<label for="newauthor">Book author:</label>'+
+  "<input id ='newauthor' type='text'><br>"+
+  '<label for="newreview">Book review:</label>'+
+  "<input id ='newreview' type='text'><br>"+
+  "<input type='submit' id='submit_thing'>"+
+  '</form>';
+
+  let bookform=document.getElementById("addbooks");
+  bookform.innerHTML=bookform;
+
+  //let fname=document.getElementById("fname").value;
+  //let lname=document.getElementById("lname").value;
+};
+
 function renderBooks(library){
-    let container = document.getElementById('books');
-    container.innerHTML = "";
+    let container = document.getElementById('yourbooks');
+    container.innerHTML = "<h4>Your books:</h4>";
     for(let book of library.books){
       let item = document.createElement('div');
       item.innerHTML= book.title+'<br>';
     container.appendChild(item);
     }
-}
+};
 
 let submit = document.getElementById('submit_thing');
 submit.addEventListener('click', async function(event){
+
   event.preventDefault();
+
   let newbook = document.getElementById('newbook').value;
   let newauthor = document.getElementById('newauthor').value;
   let newreview = document.getElementById('newreview').value;
-  /*let obj= { "title": newbook,
-        "author": newauthor,
-        "review": newreview};
-  library.books.push(obj);*/
+
   let parameters = {'newbook': newbook, 'newauthor': newauthor, 'newreview': newreview};
   let response = await fetch('http://127.0.0.1:8090/books/add', {
     method: 'POST',
@@ -33,9 +75,10 @@ submit.addEventListener('click', async function(event){
     },
     body: JSON.stringify(parameters)
   });
+
   let body = await response.json();
   renderBooks(body);
-})
+});
 
 document.getElementById('submit_search').addEventListener('click', async function(event){
   event.preventDefault();
@@ -43,7 +86,7 @@ document.getElementById('submit_search').addEventListener('click', async functio
   let response = await fetch('http://127.0.0.1:8090/books/search');
   let body = await response.json();
   searchBooks(body,searchtitle);  
-})
+});
 
 function searchBooks(library, searchtitle){
   let container = document.getElementById('body');
@@ -55,4 +98,5 @@ function searchBooks(library, searchtitle){
                         'Review:'+book.review+'<br><hr>';
         container.appendChild(item);}
   }
-}
+};
+
