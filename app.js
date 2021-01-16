d9c5ec87b5325f3adfa3c13cbb9ef0843fa61116
Adriana Tihan <fvwg34@durham.ac.uk>
2021-01-16 12:52:07 +0200

    Finished LogIn methods

--- app.js
+++ app.js
@@ -5,23 +5,19 @@ app.use(express.static('client'));
 app.use(express.json()); //Parse JSON-encoded bodies
 
 
-
-
 let rawdata1 = fs.readFileSync('./books.json');
 let library = JSON.parse(rawdata1);
 
 let rawdata2 = fs.readFileSync('./people.json');
 let people = JSON.parse(rawdata2);
 
-app.get('/books/list', function(req, resp){
-  resp.json(library);
-});
+
 
 app.get('/people/list', function(req, resp){
     resp.json(people);
 });
 
-app.post('/books/add', function(req, resp){
+/*app.post('/books/add', function(req, resp){
   const newbook = req.body.newbook;
   const newauthor = req.body.newauthor;
   const newreview = req.body.newreview;
@@ -35,6 +31,36 @@ app.post('/books/add', function(req, resp){
       }
   });
   //list.push(newbook);
+  resp.json(library);
+});*/
+
+
+app.post('/people/add', function(req, resp){
+    const fname = req.body.fname;
+    const lname = req.body.lname;
+
+    let obj= { fname: fname,
+          lname: lname};
+    
+    library.users.push(obj);
+
+    let flag=0;
+    for(let person of people.people)
+        if(person.fname==obj.fname)
+            if(person.lname==obj.lname)
+                flag=1;
+    if(flag==0 && obj.fname!='' && obj.lname!='')
+    {
+        people.people.push(obj);
+        fs.writeFile('./people.json',JSON.stringify(people, null, 2), err =>{
+           if(err){
+            console.log(err);
+            }
+    });}
+    resp.json(people);
+  });
+
+app.get('/books/list', function(req, resp){
   resp.json(library);
 });
 
--- client/index.js
+++ client/index.js
@@ -15,49 +15,49 @@ function renderPeople(people){
     }
 }
 
+
 let login = document.getElementById('LogInSubmit');
 login.addEventListener('click', async function(event){
 
   event.preventDefault();
+  document.getElementById('yourbooks').innerHTML='<button type="button" class="btn btn-primary" align="center" onclick=showBooks()>Show books</button>';
+  
+  let fname=document.getElementById('fname').value;
+  let lname=document.getElementById('lname').value;
 
+  let parameters = {'fname': fname, 'lname': lname};
+  let response = await fetch('http://127.0.0.1:8090/people/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(parameters)
+  });
+
+  let body = await response.json();
+  renderPeople(body);
+});
+
+
+async function showBooks(){
   let response = await fetch('http://127.0.0.1:8090/books/list');
   let body = await response.json();
   renderBooks(body);
-
-});
-
-function addForm(){
-
-  bookform=
-  '<h4>Add books:</h4>'+
-  "<form action='http://127.0.0.1:8090/books/add' method='post'>"+
-  '<label for="newbook">Book title:</label>'+
-  "<input id ='newbook' type='text'><br>"+
-  '<label for="newauthor">Book author:</label>'+
-  "<input id ='newauthor' type='text'><br>"+
-  '<label for="newreview">Book review:</label>'+
-  "<input id ='newreview' type='text'><br>"+
-  "<input type='submit' id='submit_thing'>"+
-  '</form>';
-
-  let bookform=document.getElementById("addbooks");
-  bookform.innerHTML=bookform;
-
-  //let fname=document.getElementById("fname").value;
-  //let lname=document.getElementById("lname").value;
-};
+}
 
 function renderBooks(library){
     let container = document.getElementById('yourbooks');
     container.innerHTML = "<h4>Your books:</h4>";
     for(let book of library.books){
-      let item = document.createElement('div');
-      item.innerHTML= book.title+'<br>';
-    container.appendChild(item);
+      if(library.users[library.users.length-1].fname==book.userfname){
+          let item = document.createElement('div');
+          item.innerHTML= book.title+'<br>';
+          container.appendChild(item);}
     }
 };
 
-let submit = document.getElementById('submit_thing');
+/*let submit = document.getElementById('submit_thing');
 submit.addEventListener('click', async function(event){
 
   event.preventDefault();
@@ -78,24 +78,30 @@ submit.addEventListener('click', async function(event){
 
   let body = await response.json();
   renderBooks(body);
-});
+});*/
 
 document.getElementById('submit_search').addEventListener('click', async function(event){
   event.preventDefault();
   let searchtitle = document.getElementById('searchtitle').value;
   let response = await fetch('http://127.0.0.1:8090/books/search');
   let body = await response.json();
-  searchBooks(body,searchtitle);  
+  searchBooks(body,searchtitle);
 });
 
 function searchBooks(library, searchtitle){
-  let container = document.getElementById('body');
-  container.innerHTML = "";
+  let container = document.getElementById('yourbooks');
+  container.innerHTML = "<h4>Search results:</h4>";
   for(let book of library.books){
     if(book.title==searchtitle){
         let item = document.createElement('div');
-        item.innerHTML= 'Reviewed by:'+'<br>'+
-                        'Review:'+book.review+'<br><hr>';
+        item.innerHTML= 'Reviewed by:'+book.userfname+ ' '+ book.userlname+ '<br>'+
+                        'Review:'+book.review+'<br>';
+        container.appendChild(item);}
+    if(book.userfname+ ' '+ book.userlname==searchtitle){
+        let item = document.createElement('div');
+        item.innerHTML= 'Title:'+ book.title+'<br>'+
+                        'Author:'+book.author+'<br>'+
+                        'Review:'+book.review+'<br>';
         container.appendChild(item);}
   }
 };
